04095d678f7901abe70ff01014a4e046333aab26
bencheatham <ben@cheatham.me>
2016-04-22 10:43:35 -0700

    working on first async call

--- index.dev.js
+++ index.dev.js
@@ -6,3 +6,11 @@ import { syncHistoryWithStore } from 'react-router-redux'
 import Root from './containers/Root'
 import configureStore from './store/configureStore'
 
+const store = configureStore()
+const history = syncHistoryWithStore(browserHistory, store)
+
+render(
+  <Root store={store} history={history} />,
+  document.getElementById('root')
+
+ )
