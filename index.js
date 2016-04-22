47f551e3d2da6110ddd0784f515275889b519ad6
bencheatham <ben@cheatham.me>
2016-04-22 10:19:33 -0700

    working on rebasing with upstream dev

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
-import 'babel-polyfill'
-import React from 'react'
-import { render } from 'react-dom'
-import { browserHistory } from 'react-router'
-import { syncHistoryWithStore } from 'react-router-redux'
-import Root from './containers/Root'
-import configureStore from './store/configureStore'
+var app = require('./server/configuration.js');
 
+var port = process.env.PORT || 8000;
+
+var path = require('path');
+var express = require('express');
+var webpack = require('webpack');
+var webpackConfig = require('./webpack.config');
+var webpackMiddleware = require('webpack-dev-middleware');
+var webpackHot = require('webpack-hot-middleware');
+var compiler = webpack(webpackConfig);
+
+if(module.parent) {
+  module.exports = app; // so we can require in tests
+} else{
+  app.listen(port);
+  console.log('Server now listening on port ' + port);
+}
+
+app.use(webpackMiddleware(compiler, {
+  quiet: true,
+  noInfo: true,
+  stats: {
+    colors: true,
+    reasons: true
+  },
+  publicPath: webpackConfig.output.publicPath
+}));
+app.use(webpackHot(compiler));
