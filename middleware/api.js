46b99d3d42ea7ac25058055d09d0cbb798b8388f
louisbuchbinder <louisbuchbinder@gmail.com>
2016-04-27 14:39:43 -0700

    automation refactor

--- middleware/api.js
+++ middleware/api.js
@@ -1,79 +1,81 @@
-import { Schema, arrayOf, normalize } from 'normalizr'
-import { camelizeKeys } from 'humps'
-import 'isomorphic-fetch'
+
+/*globals fetch, getNextPageUrl*/
+import { Schema, arrayOf, normalize } from 'normalizr';
+import { camelizeKeys } from 'humps';
+import 'isomorphic-fetch';
 
 
-const API_ROOT = 'https://api.github.com/'
+const API_ROOT = 'https://api.github.com/';
 
 
 function callApi(endpoint, schema) {
-  const fullUrl = (endpoint.indexOf(API_ROOT) === -1) ? API_ROOT + endpoint : endpoint
+  const fullUrl = (endpoint.indexOf(API_ROOT) === -1) ? API_ROOT + endpoint : endpoint;
 
   return fetch(fullUrl)
     .then(response =>
       response.json().then(json => ({ json, response}))
     ).then(({ json, response }) => {
       if (!response.ok) {
-        return Promise.reject(json)
+        return Promise.reject(json);
       }
 
-      const camelizedJson = camelizeKeys(json)
-      const nextPageUrl = getNextPageUrl(response)
+      const camelizedJson = camelizeKeys(json);
+      const nextPageUrl = getNextPageUrl(response);
 
       return Object.assign({},
         normalize(camelizedJson, schema),
         { nextPageUrl }
-      )
+      );
     }
-   )
+   );
 }
 
 const userSchema = new Schema('users', {
   idAttribute: 'login'
-})
+});
 
 export const Schemas = {
   USER: userSchema,
   USER_ARRAY: arrayOf(userSchema)
-}
+};
 
 
-export const CALL_API = Symbol('Call API')
+export const CALL_API = Symbol('Call API');
 
 export default store => next => action => {
- const callAPI = action[CALL_API]
+ const callAPI = action[CALL_API];
  if (typeof callAPI === 'undefined') {
-   return next(action)
+   return next(action);
  }
 
- let { endpoint } = callAPI
- const { schema, types } = callAPI
+ let { endpoint } = callAPI;
+ const { schema, types } = callAPI;
 
   if (typeof endpoint === 'function') {
-    endpoint = endpoint(store.getState())
+    endpoint = endpoint(store.getState());
   }
 
   if (typeof endpoint !== 'string') {
-    throw new Error('Specify a string endpoint URL.')
+    throw new Error('Specify a string endpoint URL.');
   }
   if (!schema) {
-    throw new Error('Specify one of the exported Schemas.')
+    throw new Error('Specify one of the exported Schemas.');
   }
   if (!Array.isArray(types) || types.length !== 3) {
-    throw new Error('Expected an array of three action types.')
+    throw new Error('Expected an array of three action types.');
   }
   if (!types.every(type => typeof type === 'string')) {
-    throw new Error('Expected action types to be strings.')
+    throw new Error('Expected action types to be strings.');
   }
 
   function actionWith(data) {
-    const finalAction = Object.assign({}, action, data)
-    delete finalAction[CALL_API]
-    return finalAction
+    const finalAction = Object.assign({}, action, data);
+    delete finalAction[CALL_API];
+    return finalAction;
   }
 
-  const [ requestType, successType, failureType ] = types
-  next(actionWith({ type: requestType }))
+  const [ requestType, successType, failureType ] = types;
+  next(actionWith({ type: requestType }));
 
   return callApi(endpoint, schema).then(
     response => next(actionWith({
@@ -84,5 +86,5 @@ export default store => next => action => {
       type: failureType,
       error: error.message || 'Something bad happened'
     }))
-  )
-}
+  );
+};
