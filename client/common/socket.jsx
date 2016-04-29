512a2f6fe72c3a82f39010588d07b1e6f1cee09b
Sterv <stephensulliv@gmail.com>
2016-04-28 17:08:17 -0700

    Drawer includes question component

--- client/common/socket.jsx
+++ client/common/socket.jsx
@@ -3,6 +3,5 @@ import socket from 'socket.io-client';
 if (process.env.NODE_ENV === 'production') {var io = socket().connect('https://in-class.herokuapp.com');}
 else {var io = socket().connect('http://localhost:8000');}
 
-
 export {io};
 
