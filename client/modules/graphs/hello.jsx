dd5cb5e397a9ea9828c07e015fa92111bce05690
bencheatham <ben@cheatham.me>
2016-05-07 12:37:49 -0700

    rebasing with upstream dev

---
+++ client/modules/graphs/hello.jsx
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import { BarChart } from 'react-d3';
+
+
+
+export default class Hello extends Component {
+    render() {
+        
+        var barData = [
+          {label: 'A', value: 5},
+          {label: 'B', value: 6},
+          {label: 'F', value: 7}
+        ];
+
+        return ( 
+          <div>
+            <BarChart
+                    data={barData}
+                    width={500}
+                    height={200}
+                    fill={'#3182bd'}
+                    title='Bar Chart'
+                  />;
+          </div>
+        );
+    }
+};
+ 
