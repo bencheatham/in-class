8e26acfc3effac3646dbca9040240b747b1aea4f
louisbuchbinder <louisbuchbinder@gmail.com>
2016-05-06 18:45:04 -0700

    added save and fetch answers

--- server/databaseEndpoints.js
+++ server/databaseEndpoints.js
@@ -6,10 +6,6 @@ const dbTest = require(__dirname + '/database/database.js')('test');
 
 
 
-// db.initialize(true);
-// dbTest.initialize(true);
-
-
 function verifyUsername (request, response) {
   // console.log(request.cookies);
   var auth = request.cookies.authorization; 
@@ -26,46 +22,11 @@ function verifyUsername (request, response) {
   // .catch((error) => 'error');
 }
 
-// function makeDirectory (path) {
-//   return new Promise((resolve, reject) => {
-//     // console.log('make directory');
-//     fs.mkdir(path, (error, folder) => {
-//       if (error) reject(error);
-//       resolve(true);
-//     });
-//   });
-//   // let errors fall through to a later catch    
-// }
 
-// function directoryExists (path) {
-//   return new Promise((resolve, reject) => {
-//     fs.stat(path, (error, stats) => {
-//       if (error) reject(error);
-//       else resolve(stats.isDirectory());
-//     });
-//   })
-//   .catch((error) => {console.log(error); Promise.resolve(false);});  
-// }
-
-// function fileExists (path) {
-//   return new Promise((resolve, reject) => {
-//     fs.stat(path, (error, stats) => {
-//       if (error) reject(error);
-//       else resolve(stats.isFile());
-//     });
-//   })
-//   .catch((error) => Promise.resolve(false));
-// }
 
 function fetchManifest (db, username) {
   return db.fetch('users_quizzes_join', 'title', 'username=\'' + username + '\'')
   .then((results) => results.map((result) => result.title))
-  // return new Promise((resolve, reject) => {
-  //   fs.readdir(__dirname + '/../database/json/' + username, (error, files) => { 
-  //     if (error) reject(error); 
-  //     else resolve(files.map((file) => file.slice(0,-5))); 
-  //   });
-  // })
   .catch((error) => []);
 }
 
@@ -89,6 +50,73 @@ var question = questions.shift();
 }
 
 
+const saveQuiz = (request, response, username, quiz, update, database) => {
+
+  // var directory = __dirname + '/../database/json/' + username + '/';
+  // var filePath = directory + file + '.json';
+
+  // console.log('directory: ', directory);
+
+// file must be a string
+// data must be an object
+  
+  if (quiz.title === 'manifest') { response.status(400).send('error: ' + 'manifest is a reserved filename'); return void 0; }
+  return database.fetch('quizzes', '*', 'title=\'' + quiz.title + '\'')
+  .catch((error) => [])
+  .then((existing) => {
+    // console.log('existing');
+    if (existing.length > 0) {
+      if (update) {
+        return database.deleteFrom('questions', 'title=\'' + quiz.title + '\'')
+        .then(() => database.deleteFrom('users_quizzes_join', 'title=\'' + quiz.title + '\''))
+        .then(() => database.deleteFrom('quizzes', 'title=\'' + quiz.title + '\''));
+      }
+      return Promise.reject('quiz name is not unique');
+    }
+    return 'quiz name is unique. So continue';
+  })
+  .then(() => database.insertInto('quizzes', {username: username, title: quiz.title, created: Date.now()}))
+  .then(() => database.insertInto('users_quizzes_join', {username: username, title: quiz.title}))
+  .then(() => insertQuestions(database, quiz.title, quiz.questions.slice(), username))
+  .then(() => response.status(201).send('created'))
+  .catch((error) => response.status(400).send('error: ' + 'some issue saving the quiz... Use the update flag to overwrite old quizzes'));
+
+
+};
+
+const fetchQuiz = (request, response, username, title, database) => {
+  // console.log('hello');
+  if (title === 'manifest') { 
+    // console.log('fetch manifest');
+    fetchManifest(database, username)
+    .then((files) => response.status(200).send(files));
+    return void 0;
+  }
+  return database.fetch('questions', '*', 'username=\'' + username + '\' and title=\'' + title + '\'')
+  .then((results) => {
+    var quiz = {title: results[0].title};
+    quiz.questions = results.map((result) => { return {index: result.index, question: result.question, choices: result.choices.split('+++'), answer: result.answer};});
+    return quiz;
+  })
+  .then((quiz) => response.status(200).send(quiz))
+  .catch((error) => response.status(400).send('error: ' + 'some issue fetching the quiz...'));
+};
+
+const saveAnswers = (request, response, username, answers) => {
+  return db.insertInto('answers', {teachername: answers.teachername, username: username, title: answers.title, answers: answers.answers.map(function (ans) { return '' + ans.index + ':' + ans.answer;}).join('+++'), created: Date.now()})
+  .then(() => response.status(201).send('created'))
+  .catch((error) => response.status(400).send('error: ' + 'some issue saving the answers...'));
+};
+
+const fetchAnswers = (request, response, username, title) => {
+  //username is actually the teachername in this case
+  return db.fetch('answers', 'answers', 'teachername=\'' + username + '\' and title=\'' + title + '\'')
+  .then((answers) => response.status(200).send(answers))
+  .catch((error) => response.status(400).send('error: ' + 'some issue fetching the answers...'));
+  
+};
+
+
 module.exports = (app) => {
 
   app.post('/delete', (request, response) => {
@@ -104,81 +132,66 @@ module.exports = (app) => {
 
   app.post('/save', (request, response) => {
 
+
+
     var quiz = request.body.quiz;
     var update = request.body.update;
+
+    var answers = request.body.answers;
 
     var test = request.body.test;
 
     var database = test ? dbTest : db;
 
+    var requestForQuiz = !!quiz;
+    var requestForAnswers = !!answers;
+
+    if ((requestForQuiz*1 + requestForAnswers*1) !== 1) {
+      return response.status(400).send('error: ' + 'bad request...');
+    }
 
     // console.log(file);
 
     verifyUsername(request,response)
     .catch((error) => response.status(400).send('error: ' + 'invalid token...'))
     .then((username) => {
-      
-
-      // var directory = __dirname + '/../database/json/' + username + '/';
-      // var filePath = directory + file + '.json';
-
-      // console.log('directory: ', directory);
-
-  // file must be a string
-  // data must be an object
-      
-      if (quiz.title === 'manifest') { response.status(400).send('error: ' + 'manifest is a reserved filename'); return void 0; }
-      database.fetch('quizzes', '*', 'title=\'' + quiz.title + '\'')
-      .catch((error) => [])
-      .then((existing) => {
-        // console.log('existing');
-        if (existing.length > 0) {
-          if (update) {
-            return database.deleteFrom('questions', 'title=\'' + quiz.title + '\'')
-            .then(() => database.deleteFrom('users_quizzes_join', 'title=\'' + quiz.title + '\''))
-            .then(() => database.deleteFrom('quizzes', 'title=\'' + quiz.title + '\''));
-          }
-          return Promise.reject('quiz name is not unique');
-        }
-        return 'quiz name is unique. So continue';
-      })
-      .then(() => database.insertInto('quizzes', {username: username, title: quiz.title, created: Date.now()}))
-      .then(() => database.insertInto('users_quizzes_join', {username: username, title: quiz.title}))
-      .then(() => insertQuestions(database, quiz.title, quiz.questions.slice(), username))
-      .then(() => response.status(201).send('created'))
-      .catch((error) => response.status(400).send('error: ' + 'some issue saving the quiz... Use the update flag to overwrite old quizzes'));
-
-
+      if (requestForQuiz){
+       return saveQuiz(request, response, username, quiz, update, database);
+      }
+      if (requestForAnswers) {
+        return saveAnswers(request, response, username, answers);
+      }
     });
         
   });
 
   app.get('/fetch', (request, response) => {
     var title = request.query.title;
+    var answers = request.query.answers;
     var test = request.query.test;
     var database = test ? dbTest : db;
+    
+    var requestForQuiz = !answers;
+    var requestForAnswers = !!answers;
+
+    if ((requestForQuiz*1 + requestForAnswers*1) !== 1) {
+      return response.status(400).send('error: ' + 'bad request...');
+    }
+
+
 // file must be a string
 
 
     verifyUsername(request,response)
     .catch((error) => response.status(400).send('error: ' + 'invalid token...'))
     .then((username) => {
-      // console.log('hello');
-      if (title === 'manifest') { 
-        // console.log('fetch manifest');
-        fetchManifest(database, username)
-        .then((files) => response.status(200).send(files));
-        return void 0;
+      if (requestForQuiz){
+        return fetchQuiz(request, response, username, title, database);
       }
-      return database.fetch('questions', '*', 'username=\'' + username + '\' and title=\'' + title + '\'')
-      .then((results) => {
-        var quiz = {title: results[0].title};
-        quiz.questions = results.map((result) => { return {index: result.index, question: result.question, choices: result.choices.split('+++'), answer: result.answer};});
-        return quiz;
-      })
-      .then((quiz) => response.status(200).send(quiz));
-    })
-    .catch((error) => response.status(400).send('error: ' + 'some issue fetching the quiz...'));
+      if (requestForAnswers) {
+        return fetchAnswers(request, response, username, title);
+      }
+    });
 
   });
 
