e09ea8ad46ebc75e22aa5368ee71103c3ba74765
bencheatham <ben@cheatham.me>
2016-05-12 19:57:24 -0700

    working on homepage styling.

--- client/views/HomeView.jsx
+++ client/views/HomeView.jsx
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { browserHistory } from 'react-router';
+require('../stylesheets/homeview.scss');
 
 
 export default class HomeView extends Component {
@@ -8,13 +9,17 @@ export default class HomeView extends Component {
 
  render() {
    return (
-     <div>
-       <span><h1> InClass.co - Students Stay Engaged</h1></span>
+     <div className="site-wrapper-inner">
+       <div className="cover-container">
+         <div className="inner-cover">
+         <span><h1 className="conver-heading"> InClass.co - Students Stay Engaged</h1></span>
 
-       <img 
-       src={'http://d32ogoqmya1dw8.cloudfront.net/images/NAGTWorkshops/earlycareer/teaching/large_lecture.jpg'} 
-       alt="boohoo" 
-       className="img-responsive"/>
+         <img 
+         src={'http://d32ogoqmya1dw8.cloudfront.net/images/NAGTWorkshops/earlycareer/teaching/large_lecture.jpg'} 
+         alt="boohoo" 
+         className="img-responsive"/>
+         </div>
+       </div>
     </div>
 
    );
